c05a812a0ee93a41ca0ea0e0f606a78088d8c30a
TheBigJ3 <thej3rry15@gmail.com>
2025-10-30 01:47:10 -0700

    Hot key update

--- src/Modules/Plugin/hotKey.js
+++ src/Modules/Plugin/hotKey.js
@@ -2,13 +2,13 @@ import { embedOne } from "./embeddings";
 import { retrieveTopK } from "./retrieve";
 import { buildVectorIndex, PageIndex } from "./vectorIndex";
 
-export async function onHotKey(userInstructions,currentContext) {
+export async function compileVectorSearch(userInstructions, amount, currentContext) {
     if (!PageIndex.ready) {
         await buildVectorIndex(); // build first time
     }
 
     const qv = await embedOne(userInstructions);
-    const hits = retrieveTopK(qv,20,{
+    const hits = retrieveTopK(qv,amount || 20,{
         boost: {
             focusedId: currentContext.currentFocusedElement,
             hoveredId: currentContext.currentHoveredElement,
